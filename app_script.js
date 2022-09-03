181bc44985fc787ba33565902b77c5a9eb57193d
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-03 14:22:24 +0700

    input value with declare number of colum, row, and range

--- app_script.js
+++ app_script.js
@@ -2,5 +2,5 @@ function learnGoogleAppScript() {
     var app = SpreadsheetApp;
     var spreadsheet = app.getActiveSpreadsheet();
     var activeSheet = spreadsheet.getActiveSheet();
-    activeSheet.getRange('A1:B20').setValue("Hello World!");
+    activeSheet.getRange(8, 3, 10).setValue("Hello World!");
 }
