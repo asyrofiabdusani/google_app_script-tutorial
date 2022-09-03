ebd90072127d946322594a4a61763113a26585e2
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-03 14:36:37 +0700

    get some values from range

--- app_script.js
+++ app_script.js
@@ -2,5 +2,6 @@ function learnGoogleAppScript() {
     var app = SpreadsheetApp;
     var spreadsheet = app.getActiveSpreadsheet();
     var activeSheet = spreadsheet.getActiveSheet();
-    activeSheet.getRange(8, 3, 10).setValue("Hello World!");
+    var result = activeSheet.getRange(8, 3, 5).getValues();
+    Logger.log(result);
 }
