5c953993608a1758bffab8808ee5a95382b76744
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-03 15:10:57 +0700

    using for to set the value

--- app_script.js
+++ app_script.js
@@ -2,6 +2,8 @@ function learnGoogleAppScript() {
     var app = SpreadsheetApp;
     var spreadsheet = app.getActiveSpreadsheet();
     var activeSheet = spreadsheet.getActiveSheet();
-    var result = activeSheet.getRange(8, 3, 10).getValues();
-    activeSheet.getRange(8, 4, 10).setValues(result);
+    var result = activeSheet.getRange(1, 1).getValues();
+    for (i = 0; i < 10; i++) {
+        activeSheet.getRange(i + 1, 2).setValue(result);
+    }
 }
