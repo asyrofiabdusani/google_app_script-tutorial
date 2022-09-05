862765b6ff98604c4c94a5e54d039661889dea1f
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-05 09:36:19 +0700

    working with other sheet in the same spreed sheet

--- app_script.js
+++ app_script.js
@@ -1,11 +1,4 @@
 function learnGoogleAppScript() {
-    var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    activeSheet.getRange(1, 1);
-    var lastRow = activeSheet.getLastRow();
-    var lastCol = activeSheet.getLastColumn();
-    activeSheet.getRange(lastRow + 1, 1, 1, 2).setValue("new value");
-    activeSheet.getRange(lastRow + 1, lastCol + 1, 4, 2).setValue("new value");
-    Logger.log(lastRow);
-    Logger.log(lastCol);
+    var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet2")
+    activeSheet.getRange(1, 1).setValue("hello world!")
 }
-
