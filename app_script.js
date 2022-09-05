176c62b9ca9fe0436a3f30617b16aea4cc781041
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-05 17:19:28 +0700

    sending email script

--- app_script.js
+++ app_script.js
@@ -1,15 +1,23 @@
 function learnGoogleAppScript() {
-    var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("sheet3");
-    var cal = CalendarApp.getCalendarById("asyrofiabdusani@gmail.com");
+    var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1');
     var lr = activeSheet.getLastRow();
+    var activeSheet2 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet4');
 
-    for (var i = 2; i <= lr; i++) {
-        var stDate = activeSheet.getRange(i, 1).getValue();
-        var endDate = activeSheet.getRange(i, 2).getValue();
-        var eTitl = activeSheet.getRange(i, 3).getValue();
-        var eDesc = activeSheet.getRange(i, 4).getValue();
-        var eLoc = activeSheet.getRange(i, 5).getValue();
+    var quota = (MailApp.getRemainingDailyQuota());
+    if (quota < (lr - 1)) {
+        Browser.msgBox("Email can't be sent. Remaining quota is not enough");
+    } else {
+        for (var i = 2; i <= lr; i++) {
+            var bodyTxt = activeSheet2.getRange(1, 1).getValue();
+            var email = activeSheet.getRange(i, 1).getValue();
+            var name = activeSheet.getRange(i, 2).getValue();
+            var subj = activeSheet.getRange(i, 3).getValue();
+            var bill = activeSheet.getRange(i, 4).getDisplayValue();
+            var dueDate = activeSheet.getRange(i, 5).getDisplayValue();
 
-        cal.createEvent(eTitl, stDate, endDate, { location: eLoc, description: eDesc });
+            bodyTxt = bodyTxt.replace('{name}', name).replace('{bill}', bill).replace('{date}', dueDate);
+
+            MailApp.sendEmail(email, subj, bodyTxt);
+        }
     }
 }
