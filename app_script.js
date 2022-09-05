56f3ea8c79ff1e853a92ff83504f65de864c6f18
Asyrofi Abdusani <asyrofiabdusani@gmail.com>
2022-09-05 22:06:24 +0700

    object in app script

--- app_script.js
+++ app_script.js
@@ -1,23 +1,11 @@
 function learnGoogleAppScript() {
-    var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1');
-    var lr = activeSheet.getLastRow();
-    var activeSheet2 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet4');
+    var dataObj = {
+        name: 'Roni',
+        age: 36,
+        address: '4th avenue,  New York'
+    }
 
-    var quota = (MailApp.getRemainingDailyQuota());
-    if (quota < (lr - 1)) {
-        Browser.msgBox("Email can't be sent. Remaining quota is not enough");
-    } else {
-        for (var i = 2; i <= lr; i++) {
-            var bodyTxt = activeSheet2.getRange(1, 1).getValue();
-            var email = activeSheet.getRange(i, 1).getValue();
-            var name = activeSheet.getRange(i, 2).getValue();
-            var subj = activeSheet.getRange(i, 3).getValue();
-            var bill = activeSheet.getRange(i, 4).getDisplayValue();
-            var dueDate = activeSheet.getRange(i, 5).getDisplayValue();
-
-            bodyTxt = bodyTxt.replace('{name}', name).replace('{bill}', bill).replace('{date}', dueDate);
-
-            MailApp.sendEmail(email, subj, bodyTxt);
-        }
+    for (var key in dataObj) {
+        Logger.log(key + ' = ' + dataObj[key]);
     }
 }
